e69dcf5f236a6a2c397a204db5db3d690b037bb6
Vishal Kumar Varma <sirikondavishal8055@gmail.com>
2020-08-31 00:28:14 +0530

    ,.

---
+++ Android multiPlay/Assets/This Game/Scripts/MyCamera.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCamera : MonoBehaviour
+{
+  
+    public float X_Pole;     //equall to Y-axis rotation in main camera
+    
+    public float  Y_Pole;    // //equall to X-axis rotation in main camera
+    public float Camera_dist = 2f;
+    
+    public Vector2 Orbit_Sensitive = new Vector2(4f, -4f);
+
+    public Transform targetbody;
+    public Vector3 target_offset_Look = new Vector3(0, 0.85f, 0);
+
+    Vector3 _cam_Position;
+   
+
+    private void Awake()
+    {
+        targetbody = GameObject.FindGameObjectWithTag("Player").transform;
+        _cam_Position = Camera.main.transform.localPosition;
+        
+    }
+    void Update()
+    {
+        X_Pole += Input.GetAxis("Mouse Y") * Orbit_Sensitive.y;
+        Y_Pole += Input.GetAxis("Mouse X") * Orbit_Sensitive.x;
+
+        Vector3 camRotate = new Vector3(X_Pole, Y_Pole);
+        transform.eulerAngles = camRotate;
+
+
+        transform.position = targetbody.position - transform.forward * Camera_dist;
+      
+
+        transform.LookAt(targetbody.position + target_offset_Look);
+    }
+}//class
---
+++ Android multiPlay/Assets/This Game/Scripts/MyPlayer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyPlayer : MonoBehaviour
+{
+    public float MoveSpeed = 3f;
+    public float smoothRotationTime = 5f;
+    public float SmoothSpeedMove = 1f;
+    
+    float rotateVelocity;
+
+    float currentSpeed;
+    float speedVelocity;
+
+   
+
+    private void Start()
+    {
+
+    }
+    void Update()
+    {
+        //Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        //Vector2 inputDir = input.normalized;
+
+        //if (inputDir != Vector2.zero)
+        //{ 
+        //    float rotation =  Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg;
+        //    transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, rotation, ref currentVeclocity, smoothRotationTime);
+        //}
+        //float tragetSpeed = MoveSpeed * inputDir.magnitude;
+        //currentSpeed = Mathf.SmoothDamp(currentSpeed, tragetSpeed, ref speedVelocity, 0.1f);
+
+        //transform.Translate(transform.forward * currentSpeed * Time.deltaTime, Space.World);
+
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 inputDir = input.normalized;
+
+        if (inputDir != Vector2.zero)
+        {
+            float angle = Mathf.Atan2(inputDir.x, inputDir.y) /*  <<== gives radians only*/  * Mathf.Rad2Deg;
+
+            // vector3 UP becos we want to rotate on Y-Axix
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, angle, ref rotateVelocity, smoothRotationTime * Time.smoothDeltaTime);
+        }
+
+        float target = MoveSpeed * inputDir.magnitude;
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, target, ref speedVelocity,SmoothSpeedMove*Time.smoothDeltaTime );
+
+        transform.Translate(transform.forward * currentSpeed  * Time.smoothDeltaTime, Space.World);
+
+       // print(transform.forward);
+       // print(currentSpeed);
+    } 
+}
+
