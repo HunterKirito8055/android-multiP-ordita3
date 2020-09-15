81c1c6fb7e877efaf0876d4025b671855c248a12
Vishal Kumar Varma <sirikondavishal8055@gmail.com>
2020-09-15 13:21:51 +0530

    again downoload

--- Android multiPlay/Assets/This Game/Scripts/MyCamera.cs
+++ Android multiPlay/Assets/This Game/Scripts/MyCamera.cs
@@ -9,33 +9,37 @@ public class MyCamera : MonoBehaviour
     
     public float  Y_Pole;    // //equall to X-axis rotation in main camera
     public float Camera_dist = 2f;
-    
-    public Vector2 Orbit_Sensitive = new Vector2(4f, -4f);
+    public Vector2 ClampArea = new Vector2(-40f, 80f);
+    public Vector2 Orbit_Sensitive = new Vector2(4f, 4f);
 
     public Transform targetbody;
     public Vector3 target_offset_Look = new Vector3(0, 0.85f, 0);
 
     Vector3 _cam_Position;
+    Vector3 _refvelocity;
+    Vector3 camRotate;
+    public float smoothMoveTime  = 0.12f;
    
-
     private void Awake()
     {
         targetbody = GameObject.FindGameObjectWithTag("Player").transform;
         _cam_Position = Camera.main.transform.localPosition;
         
     }
-    void Update()
+    void LateUpdate()
     {
-        X_Pole += Input.GetAxis("Mouse Y") * Orbit_Sensitive.y;
+        X_Pole -= Input.GetAxis("Mouse Y") * Orbit_Sensitive.y;
         Y_Pole += Input.GetAxis("Mouse X") * Orbit_Sensitive.x;
 
-        Vector3 camRotate = new Vector3(X_Pole, Y_Pole);
+        X_Pole = Mathf.Clamp(X_Pole,ClampArea.x, ClampArea.y);
+
+        camRotate = Vector3.SmoothDamp(camRotate, new Vector3(X_Pole, Y_Pole), ref _refvelocity, smoothMoveTime);
         transform.eulerAngles = camRotate;
 
 
         transform.position = targetbody.position - transform.forward * Camera_dist;
       
 
-        transform.LookAt(targetbody.position + target_offset_Look);
+       transform.LookAt(targetbody.position + target_offset_Look);
     }
 }//class
--- Android multiPlay/Assets/This Game/Scripts/MyPlayer.cs
+++ Android multiPlay/Assets/This Game/Scripts/MyPlayer.cs
@@ -1,58 +1,55 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MyPlayer : MonoBehaviour
 {
     public float MoveSpeed = 3f;
     public float smoothRotationTime = 5f;
-    public float SmoothSpeedMove = 1f;
-    
+    public float SmoothSpeedMove = 5f;
+
+   // public Transform cameraTrans;
+
     float rotateVelocity;
 
     float currentSpeed;
     float speedVelocity;
+    public bool MobileInputs = false;
 
-   
+    FixedJoystick joystick;
 
     private void Start()
     {
-
+     
     }
     void Update()
     {
-        //Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        //Vector2 inputDir = input.normalized;
-
-        //if (inputDir != Vector2.zero)
-        //{ 
-        //    float rotation =  Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg;
-        //    transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, rotation, ref currentVeclocity, smoothRotationTime);
-        //}
-        //float tragetSpeed = MoveSpeed * inputDir.magnitude;
-        //currentSpeed = Mathf.SmoothDamp(currentSpeed, tragetSpeed, ref speedVelocity, 0.1f);
-
-        //transform.Translate(transform.forward * currentSpeed * Time.deltaTime, Space.World);
-
-        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        Vector2 inputDir = input.normalized;
-
-        if (inputDir != Vector2.zero)
+        Vector3 input = Vector3.zero;
+        if (MobileInputs)
         {
-            float angle = Mathf.Atan2(inputDir.x, inputDir.y) /*  <<== gives radians only*/  * Mathf.Rad2Deg;
+             input = new Vector3(Input.GetAxis("Joystick X"), Input.GetAxis("Joystick Y"));
+        }
+        else
+        {
+            input = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        }
+
+        Vector3 inputDir = input.normalized;
+
+        if (inputDir != Vector3.zero)
+        {
+            /*  this bellow mathf.atan2 gives radians only*/
+            float angle = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
 
             // vector3 UP becos we want to rotate on Y-Axix
             transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, angle, ref rotateVelocity, smoothRotationTime * Time.smoothDeltaTime);
         }
 
         float target = MoveSpeed * inputDir.magnitude;
-        currentSpeed = Mathf.SmoothDamp(currentSpeed, target, ref speedVelocity,SmoothSpeedMove*Time.smoothDeltaTime );
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, target, ref speedVelocity, SmoothSpeedMove * Time.smoothDeltaTime);
 
-        transform.Translate(transform.forward * currentSpeed  * Time.smoothDeltaTime, Space.World);
+        transform.Translate(transform.forward * currentSpeed * Time.smoothDeltaTime, Space.World);
 
-       // print(transform.forward);
-       // print(currentSpeed);
-    } 
+        // print(transform.forward);
+        // print(currentSpeed);
+    }
 }
 
